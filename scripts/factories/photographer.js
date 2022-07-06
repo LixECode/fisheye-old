2a85c1e1200438b4952e60fbed6ecf5b571004d0
LixECode <101730639+LixECode@users.noreply.github.com>
2022-07-06 15:44:24 +0200

    etpae 1 ok

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,38 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const {
+        name,
+        city,
+        country,
+        tagline,
+        price,
+        portrait
+    } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement("article");
+        const img = document.createElement("img");
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        // créer un a pour redirection
         article.appendChild(img);
+
+        const h2 = document.createElement("h2");
+        h2.textContent = name;
         article.appendChild(h2);
+        const text = document.createElement("p");
+        text.innerHTML = `<div class="article_information">
+      <p class="city">${city + ", " + country}</p>
+      <p class="tagline">${tagline}</p>
+      <p class="price">^${price}€/jour</p>
+      </div>`
+        article.appendChild(text);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return {
+        name,
+        picture,
+        getUserCardDOM
+    }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,33 @@
 async function getPhotographers() {
-
-    fetch('data/photographers.json')
+    let photographers = [];
+    await fetch("/data/photographers.json")
         .then(function (response) {
             if (response.ok) {
                 return response.json();
             }
         })
 
-        .then(function (photographers) {
-            console.log(photographers);
-            return `
+        .then(function (data) {
+            if (data.value == photographers) {
+                console.log(photographers);
+                return photographers;
+            }
+        });
+
+    `
         <article>
-            <div class=info tabindex="2">
+            < div class="photographer_section">
                 <h2>${name}</h2>
                 <h3>${city}, ${country}</h3>
                 <p>${tagline}</p>
             </div>
                 <img src="${picture}" alt="${name}"
             </article>`
-        })
 
-        .catch(function (error) {
-            // erreur survenue
-        })
+    .catch(function (error) {
+        // erreur survenue
+    })
 };
-
-// Penser à remplacer par les données récupérées dans le json
-const photographers = {
-    name,
-    portrait,
-    city,
-    country,
-    tagline,
-}
-
-const picture = `/src/assets/photographers/${portrait}`;
-
-// // et bien retourner le tableau photographers seulement une fois
-// return ({
-//     photographers
-// });
-
 
 
 

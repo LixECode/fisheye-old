358a84502491dfd13694d32be72ebfe8f0c1e9b9
LixECode <101730639+LixECode@users.noreply.github.com>
2022-07-12 15:11:41 +0200

    factory ok linked html in progresse

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,3 +1,5 @@
+// FACTORY 1
+
 function photographerFactory(data) {
     const {
         name,
@@ -25,9 +27,184 @@ function photographerFactory(data) {
         return (article);
     }
 
+    // FACTORY PHOTOGRAPHER 1
+
+    function getPortraitPhotographer() {
+        const section = document.createElement('section')
+        section.classList.add('photograph-section')
+        section.innerHTML =
+            `
+    <div class="photograph-header">
+    <h2 class="name-portrait">${name}</h2>
+    <p class="city-portrait">${city}, ${country}</p>
+    <p class="tagline-portrait">${tagline}</p>
+    </div>
+    <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+    <img class="img-portrait" src="${picture}" alt="${id}" title="picture of" + "${id}">
+        `
+        return (section)
+    }
+
     return {
         name,
+        id,
+        city,
+        country,
+        tagline,
+        price,
+        portrait,
         picture,
-        getUserCardDOM
+        getUserCardDOM,
+        getPortraitPhotographer
     }
+}
+
+// FACTORY PHOTOGRAPHER 2
+
+function photographerMedia(data) {
+    const {
+        photographerId,
+        title,
+        image,
+        likes,
+    } = data;
+
+    const images = `assets/images/${photographerId}/${image}`;
+
+    function getPhotographerMedia() {
+        const media = document.createElement('section')
+        media.classList.add('photograph-filter')
+        media.innerHTML =
+            `
+      <div class="filter-header">
+        <label for="filter-menu">Trier par</label><br>
+        <select name="filters" id="filters-select">
+          <option class="filter-popularity" value="popularity">Popularité</option>
+          <option class="filter-date" value="date">Date</option>
+          <option class="filter-title" value="title">Titre</option>
+        </select>
+      </div>
+
+      <div class="filter-container">
+        <article class="filter-article">
+          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
+          <div class="filter-text">
+            <h3>${title}</h3>
+            <ul class="photograph-like">
+              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
+            </ul>
+          </div>
+        </article>
+        <article class="filter-article">
+          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
+          <div class="filter-text">
+            <h3>$${title}</h3>
+            <ul class="photograph-like">
+              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
+            </ul>
+          </div>
+        </article>
+        <article class="filter-article">
+          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
+          <div class="filter-text">
+            <h3>${title}</h3>
+            <ul class="photograph-like">
+              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
+            </ul>
+          </div>
+        </article>
+        <article class="filter-article">
+          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
+          <div class="filter-text">
+            <h3>${title}</h3>
+            <ul class="photograph-like">
+              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
+            </ul>
+          </div>
+        </article>
+        <article class="filter-article">
+          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
+          <div class="filter-text">
+            <h3>${title}</h3>
+            <ul class="photograph-like">
+              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
+            </ul>
+          </div>
+        </article>
+        <article class="filter-article">
+          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
+          <div class="filter-text">
+            <h3>${title}</h3>
+            <ul class="photograph-like">
+              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
+            </ul>
+          </div>
+        </article>
+        <article class="filter-article">
+          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
+          <div class="filter-text">
+            <h3>${title}</h3>
+            <ul class="photograph-like">
+              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
+            </ul>
+          </div>
+        </article>
+        <article class="filter-article">
+          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
+          <div class="filter-text">
+            <h3>${title}</h3>
+            <ul class="photograph-like">
+              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
+            </ul>
+          </div>
+        </article>
+        <article class="filter-article">
+          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
+          <div class="filter-text">
+            <h3>${title}</h3>
+            <ul class="photograph-like">
+              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
+            </ul>
+          </div>
+        </article>
+      </div>
+    `
+        return (section)
+    }
+    return {
+        photographerId,
+        title,
+        image,
+        likes,
+        getPhotographerMedia
+    }
+}
+
+// FACTORY PHOTOGRAPHER NAV
+
+function getUserNav(data) {
+    const {
+        price,
+        likes
+    } = data;
+
+    function photographerNav() {
+
+        const nav = document.createElement('div')
+        nav.classList.add('photograph-nav')
+        nav.innerHTML =
+            `
+      <ul class="photograph-like">
+        <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
+      </ul>
+      <p>${price}€/jour</p>
+          `
+        return (nav)
+    }
+    return {
+        price,
+        likes,
+        photographerNav
+    }
+
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -5,176 +5,54 @@
 // console.log(id)
 
 let str = new URLSearchParams(window.location.search);
-let id = str.get("id");
-console.log(id)
+let urlId = str.get("id");
 
-// faire un fetch photographers comme home
 async function getMedia() {
-    let media = [];
-    await fetch("/data/photographers.json")
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            }
-        })
+  let media = [];
+  let photographers = [];
+  await fetch("/data/photographers.json")
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+    })
 
-        .then(function (data) {
-            media = data.media
-        })
-    console.log(media);
-    return {
-        media
-    };
+    .then(function (data) {
+      media = data.media
+      photographers = data.photographers
+    })
+  return {
+    media,
+    photographers
+  };
 }
 
 // filtrer par ID en 2 fois (.then) ou fonction à part appelée dans le .then //
 
 
-function photographerMedia(data) {
-    const {
-        id,
-        photographerId,
-        title,
-        image,
-        likes,
-        date,
-        price
-    } = data;
+async function init() {
+  // Récupère les datas des photographes
+  const {
+    media,
+    photographers
+  } = await getMedia();
+  displayDataPhotographer(photographers, media)
+};
 
-    // A CHECKER LA REDIRECTION
-    const images = `assets/images/${photographerId}/${image}`;
+init();
+// Afficher les informations data
+async function displayDataPhotographer(photographers, media) {
+  const photographersMain = document.querySelector('#main')
+  console.log(urlId)
+  console.log(photographers)
 
-    // A CHECKER CAR CA AFFICHE RIEN
-    // j'ai créer une div factice pour mettre mes deux sections dedans mais ça n'affiche rien
-    // j'ai gardé le html en comments dans la page html
-    function getUserMediaDOM() {
-        const div = document.createElement('div')
-        div.innerHTML =
-            `
-    <section class="photograph-section">
-   
-    <div class="photograph-header">
-    <h2 class="name-portrait">${id}</h2>
-    <p class="city-portrait">${city}, ${country}</p>
-    <p class="tagline-portrait">${tagline}</p>
-    </div>
-    <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
-    <img class="img-portrait" src="${picture}" alt="${id}" title="picture of" + "${id}">
-    
-    </section>
+  const photographer = photographers.find(function (findId) {
+    return findId.id == urlId
+  });
+  console.log(photographer)
+  const photographerModel = photographerFactory(photographer);
+  const userPortrait = photographerModel.getPortraitPhotographer();
+  photographersMain.appendChild(userPortrait);
 
-    <section class="photograph-filter">
-
-      <div class="filter-header">
-        <label for="filter-menu">Trier par</label><br>
-        <select name="filters" id="filters-select">
-          <option class="filter-popularity" value="popularity">Popularité</option>
-          <option class="filter-date" value="date">Date</option>
-          <option class="filter-title" value="title">Titre</option>
-        </select>
-      </div>
-
-      <div class="filter-container">
-        <article class="filter-article">
-          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
-          <div class="filter-text">
-            <h3>${title}</h3>
-            <ul class="photograph-like">
-              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
-            </ul>
-          </div>
-        </article>
-        <article class="filter-article">
-          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
-          <div class="filter-text">
-            <h3>$${title}</h3>
-            <ul class="photograph-like">
-              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
-            </ul>
-          </div>
-        </article>
-        <article class="filter-article">
-          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
-          <div class="filter-text">
-            <h3>${title}</h3>
-            <ul class="photograph-like">
-              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
-            </ul>
-          </div>
-        </article>
-        <article class="filter-article">
-          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
-          <div class="filter-text">
-            <h3>${title}</h3>
-            <ul class="photograph-like">
-              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
-            </ul>
-          </div>
-        </article>
-        <article class="filter-article">
-          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
-          <div class="filter-text">
-            <h3>${title}</h3>
-            <ul class="photograph-like">
-              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
-            </ul>
-          </div>
-        </article>
-        <article class="filter-article">
-          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
-          <div class="filter-text">
-            <h3>${title}</h3>
-            <ul class="photograph-like">
-              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
-            </ul>
-          </div>
-        </article>
-        <article class="filter-article">
-          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
-          <div class="filter-text">
-            <h3>${title}</h3>
-            <ul class="photograph-like">
-              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
-            </ul>
-          </div>
-        </article>
-        <article class="filter-article">
-          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
-          <div class="filter-text">
-            <h3>${title}</h3>
-            <ul class="photograph-like">
-              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
-            </ul>
-          </div>
-        </article>
-        <article class="filter-article">
-          <img src="${images}" alt="${title}" title= "picture of" + "${title}">
-          <div class="filter-text">
-            <h3>${title}</h3>
-            <ul class="photograph-like">
-              <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
-            </ul>
-          </div>
-        </article>
-      </div>
-    </section>
-    
- <div class="photograph-nav">
-      <ul class="photograph-like">
-        <li>${likes}<i class="fa-solid fa-heart heartSolid"></i></li>
-      </ul>
-      <p>${price}€/jour</p>
-    </div>
-          `
-        return (div);
-    }
-
-    return {
-        image,
-        title,
-        price,
-        likes,
-        getUserMediaDOM
-    }
-
-}
+  //tableau media
+};
